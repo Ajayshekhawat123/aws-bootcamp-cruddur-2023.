d5b0cb3aa65134d3d8d04283b70da273a480b59c
Ajay Singh Shekhawat <ajay.shekhawat@nexulus.in>
2023-04-03 10:54:29 +0000

    instrument honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
-from services.notifications_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -13,7 +13,30 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# HoneyComb ---------
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# HoneyComb ---------
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = Flask(__name__)
+
+# HoneyComb ---------
+# Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -24,9 +47,6 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
-@app.route('/health')
-def health_check():
-    return jsonify(status='healthy'), 200
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -68,10 +88,13 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
-@app.route("/api/activities/notifications", methods=['GET'])
-def data_notifications():
-  data = NotificationsActivities.run()
-  return data, 200
+@app.route("/api/activities/@<string:handle>", methods=['GET'])
+def data_handle(handle):
+  model = UserActivities.run(handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
 @app.route("/api/activities/search", methods=['GET'])
 def data_search():
